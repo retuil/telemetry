af456fc14fac733cb71c426b92aa34ba2b061abe
Сурганов Денис Кириллович <d.surganov@modulbank.ru>
2025-10-28 16:00:48 +0500

    Задание Сурганов Романов Рыбин

--- Program.cs
+++ Program.cs
@@ -1,7 +1,34 @@
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+using telemetry;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+
+builder.Services.AddSingleton<Metrics>();
+
+builder.Logging.AddOpenTelemetry(options =>
+{
+    options
+        .SetResourceBuilder(
+            ResourceBuilder.CreateDefault()
+                .AddService("TelemetryExample"))
+        .AddConsoleExporter();
+});
+builder.Services.AddOpenTelemetry()
+    .ConfigureResource(resource => resource.AddService("TelemetryExample"))
+    .WithTracing(tracing => tracing
+        .AddAspNetCoreInstrumentation()
+        .AddConsoleExporter())
+    .WithMetrics(metrics => metrics
+        .AddPrometheusExporter()
+        .AddAspNetCoreInstrumentation()
+        .AddConsoleExporter()
+        .AddMeter(nameof(Metrics)));
 
 var app = builder.Build();
 
@@ -21,5 +48,7 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapRazorPages();
+
+app.UseOpenTelemetryPrometheusScrapingEndpoint();
 
 app.Run();
